d53b2a12bc176a76fe7945436cf033c3f0b8ea1d
Andrii Smidnov <yukihirosm@gmail.com>
2023-04-01 20:27:37 +0300

    Add timestamps to logging

--- core/settings.py
+++ core/settings.py
@@ -114,31 +114,35 @@ TEMPLATES = [
 
 LOGGING = {
     'version': 1,
-    # The version number of our log
     'disable_existing_loggers': False,
-    # django uses some of its own loggers for internal operations. In case you want to disable them just replace the False above with true.
-    # A handler for WARNING. It is basically writing the WARNING messages into a file called WARNING.log
     'handlers': {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': f"{BASE_DIR}/DEBUG.log",
+            'filename': f"{BASE_DIR}/logging.log",
+            'formatter': 'timestamped',
         },
         'console': {
             'level': 'DEBUG',
-            'class': 'logging.StreamHandler'
+            'class': 'logging.StreamHandler',
+            'formatter': 'timestamped',
         },
     },
-    # A logger for WARNING which has a handler called 'file'. A logger can have multiple handler
     'loggers': {
-       # notice the blank '', Usually you would put built in loggers like django or root here based on your needs
         '': {
-            'handlers': ['console'], #notice how file variable is called in handler which has been defined above
+            'handlers': ['file'],
             'level': 'DEBUG',
             'propagate': True,
         },
     },
+    'formatters': {
+        'timestamped': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+    },
 }
+
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
