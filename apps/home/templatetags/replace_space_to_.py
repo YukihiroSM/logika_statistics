263a1ccd7cbaec0a7b366cc914fc7ebeb5e125e6
Andrii Smidonov <yukihirosm@gmail.com>
2023-04-06 18:05:12 +0300

    Update replace templatetag

--- apps/home/templatetags/replace_space_to_.py
+++ apps/home/templatetags/replace_space_to_.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 @register.filter
 def replace_space_to_(value):
-    return value.replace(" ", "_").replace("'", "")
+    return value.replace(" ", "_").replace("'", "") if value else ""
 
