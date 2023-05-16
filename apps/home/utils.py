5665931db27fd91eb521180de24b66d2203f527c
Andrii Smidonov <yukihirosm@gmail.com>
2023-05-16 23:06:27 +0300

    Update codestyle + remove unused buttons

--- apps/home/utils.py
+++ apps/home/utils.py
@@ -11,41 +11,53 @@ result_extended = []
 auth = library.lms_auth()
 
 
-def retrieve_group_ids_from_csv(auth: requests.Session(), report_start: str, report_end: str) -> list:
+def retrieve_group_ids_from_csv(
+    auth: requests.Session(), report_start: str, report_end: str
+) -> list:
     ids = []
-    resp = auth.get(f"https://lms.logikaschool.com/group/default/schedule?GroupWithLessonSearch%5BnextLessonTime%5D={report_start}+-+{report_end}&GroupWithLessonSearch%5Bid%5D=&GroupWithLessonSearch%5Btitle%5D=&GroupWithLessonSearch%5Bvenue%5D=&GroupWithLessonSearch%5Bactive_student_count%5D=&GroupWithLessonSearch%5Bweekday%5D=&GroupWithLessonSearch%5Bteacher%5D=&GroupWithLessonSearch%5Bcurator%5D=&GroupWithLessonSearch%5Btype%5D=&GroupWithLessonSearch%5Bcourse%5D=&GroupWithLessonSearch%5Bbranch%5D=&export=true&name=default&exportType=csv")
+    resp = auth.get(
+        f"https://lms.logikaschool.com/group/default/schedule?GroupWithLessonSearch%5BnextLessonTime%5D={report_start}+-+{report_end}&GroupWithLessonSearch%5Bid%5D=&GroupWithLessonSearch%5Btitle%5D=&GroupWithLessonSearch%5Bvenue%5D=&GroupWithLessonSearch%5Bactive_student_count%5D=&GroupWithLessonSearch%5Bweekday%5D=&GroupWithLessonSearch%5Bteacher%5D=&GroupWithLessonSearch%5Bcurator%5D=&GroupWithLessonSearch%5Btype%5D=&GroupWithLessonSearch%5Bcourse%5D=&GroupWithLessonSearch%5Bbranch%5D=&export=true&name=default&exportType=csv"
+    )
     if resp.status_code == ok_status:
-        decoded_content = resp.content.decode('utf-8')
-        csv_file = csv.reader(decoded_content.splitlines(), delimiter=';')
+        decoded_content = resp.content.decode("utf-8")
+        csv_file = csv.reader(decoded_content.splitlines(), delimiter=";")
         data = list(csv_file)
         for row in data:
             ids.append(row[1])
     else:
-        print(f'Bad request! Response [{resp.status_code}].')
+        print(f"Bad request! Response [{resp.status_code}].")
     return ids
 
 
 def parse_groups_portion(group_ids):
     global result
     for group_id in group_ids:
-        group_data_response = auth.get(f"https://lms.logikaschool.com/api/v1/group/{group_id}?expand=venue,teacher,curator,branch")
-        retrieve_link = auth.get(f"https://lms.logikaschool.com/api/v2/group/online/view/{group_id}")
+        group_data_response = auth.get(
+            f"https://lms.logikaschool.com/api/v1/group/{group_id}?expand=venue,teacher,curator,branch"
+        )
+        retrieve_link = auth.get(
+            f"https://lms.logikaschool.com/api/v2/group/online/view/{group_id}"
+        )
         if group_data_response.status_code and retrieve_link.status_code == ok_status:
             if group_data_response.json() and retrieve_link.json() is not None:
-                data = group_data_response.json().get('data', dict())
-                link_data = retrieve_link.json().get('data', dict)
-                backup_online_room_url = link_data.get('backupOnlineRoomUrl', dict())
-                link = backup_online_room_url.get('url')
-                next_lesson_time = data.get('next_lesson_time')
-                teacher = data.get('teacher', dict())
-                teacher_name = teacher.get('name')
-                result.append({'group': group_id,
-                               'link': link,
-                               'next_lesson_time': next_lesson_time,
-                               'teacher_name': teacher_name})
+                data = group_data_response.json().get("data", dict())
+                link_data = retrieve_link.json().get("data", dict)
+                backup_online_room_url = link_data.get("backupOnlineRoomUrl", dict())
+                link = backup_online_room_url.get("url")
+                next_lesson_time = data.get("next_lesson_time")
+                teacher = data.get("teacher", dict())
+                teacher_name = teacher.get("name")
+                result.append(
+                    {
+                        "group": group_id,
+                        "link": link,
+                        "next_lesson_time": next_lesson_time,
+                        "teacher_name": teacher_name,
+                    }
+                )
 
             else:
-                print(f'Bad request! Response [{group_data_response.status_code}].')
+                print(f"Bad request! Response [{group_data_response.status_code}].")
 
 
 def parse_in_threads(group_ids):
@@ -56,9 +68,9 @@ def parse_in_threads(group_ids):
     args = []
     for i in range(0, num_of_threads):
         if i == num_of_threads - 1:
-            arg = group_ids[i * separator:]
+            arg = group_ids[i * separator :]
         else:
-            arg = group_ids[i * separator:(i + 1) * separator]
+            arg = group_ids[i * separator : (i + 1) * separator]
         args.append(arg)
     with ThreadPoolExecutor(max_workers=num_of_threads) as executor:
         executor.map(parse_groups_portion, args)
@@ -74,17 +86,21 @@ def get_lessons_links(ids: list) -> list:
 def parse_groups_portion_extended(group_ids):
     global result_extended
     for group_id in group_ids:
-        group_data_response = auth.get(f"https://lms.logikaschool.com/api/v1/group/{group_id}?expand=venue,teacher,curator,branch")
-        retrieve_link = auth.get(f"https://lms.logikaschool.com/api/v2/group/online/view/{group_id}")
+        group_data_response = auth.get(
+            f"https://lms.logikaschool.com/api/v1/group/{group_id}?expand=venue,teacher,curator,branch"
+        )
+        retrieve_link = auth.get(
+            f"https://lms.logikaschool.com/api/v2/group/online/view/{group_id}"
+        )
         if group_data_response.status_code and retrieve_link.status_code == ok_status:
             if group_data_response.json() and retrieve_link.json() is not None:
-                data = group_data_response.json().get('data', dict())
-                link_data = retrieve_link.json().get('data', dict())
-                backup_online_room_url = link_data.get('backupOnlineRoomUrl', dict())
-                link = backup_online_room_url.get('url')
-                next_lesson_time = data.get('next_lesson_time')
-                teacher = data.get('teacher', dict())
-                teacher_name = teacher.get('name')
+                data = group_data_response.json().get("data", dict())
+                link_data = retrieve_link.json().get("data", dict())
+                backup_online_room_url = link_data.get("backupOnlineRoomUrl", dict())
+                link = backup_online_room_url.get("url")
+                next_lesson_time = data.get("next_lesson_time")
+                teacher = data.get("teacher", dict())
+                teacher_name = teacher.get("name")
                 group_name = data.get("title")
                 curator = data.get("curator")
                 if curator:
@@ -97,16 +113,20 @@ def parse_groups_portion_extended(group_ids):
                     course_name = course.get("name")
                 else:
                     course_name = "Відсутній курс в ЛМС"
-                result_extended.append({'group': group_id,
-                                        'link': link,
-                                        'next_lesson_time': next_lesson_time,
-                                        'teacher_name': teacher_name,
-                                        "group_name": group_name,
-                                        "curator_name": curator_name,
-                                        "course_name": course_name})
+                result_extended.append(
+                    {
+                        "group": group_id,
+                        "link": link,
+                        "next_lesson_time": next_lesson_time,
+                        "teacher_name": teacher_name,
+                        "group_name": group_name,
+                        "curator_name": curator_name,
+                        "course_name": course_name,
+                    }
+                )
 
             else:
-                print(f'Bad request! Response [{group_data_response.status_code}].')
+                print(f"Bad request! Response [{group_data_response.status_code}].")
 
 
 def parse_in_threads_extended(group_ids):
@@ -117,9 +137,9 @@ def parse_in_threads_extended(group_ids):
     args = []
     for i in range(0, num_of_threads):
         if i == num_of_threads - 1:
-            arg = group_ids[i * separator:]
+            arg = group_ids[i * separator :]
         else:
-            arg = group_ids[i * separator:(i + 1) * separator]
+            arg = group_ids[i * separator : (i + 1) * separator]
         args.append(arg)
     with ThreadPoolExecutor(max_workers=num_of_threads) as executor:
         executor.map(parse_groups_portion_extended, args)
@@ -130,3 +150,39 @@ def get_lessons_links_extended(ids: list) -> list:
     result_extended = []
     parse_in_threads_extended(ids)
     return result_extended
+
+
+def collect_groups_by_teacher_location(
+    teacher_name: str | None = None, location: str | None = None
+) -> list[dict]:
+    link_for_groups = "https://lms.logikaschool.com/api/v1/group"
+    resp = auth.get(link_for_groups)
+    if resp.status_code == ok_status:
+        data = resp.json().get("data", dict())
+        filtered_groups = []
+        for group in data.get("items"):
+            if teacher_name and group.get("teacherName") != teacher_name:
+                continue
+            if location and group.get("venue") != location:
+                continue
+            group_type = group.get("type")
+            group_type_label = group_type.get("label")
+            group_status = group.get("status")
+            group_status_label = group_status.get("label")
+            if group_type_label == "Демо" or (
+                group_status_label != "Активная" and group_status_label != "Идет набор"
+            ):
+                continue
+            group_id = group.get("id")
+            group_name = group.get("title")
+            group_data = {"group_id": group_id, "group_name": group_name}
+            filtered_groups.append(group_data)
+        return filtered_groups
+
+
+def collect_groups_schedule():
+    teacher_name = "Александр Гурчин"
+    location = "Онлайн"
+    teacher_groups = collect_groups_by_teacher_location(
+        teacher_name=teacher_name, location=location
+    )
