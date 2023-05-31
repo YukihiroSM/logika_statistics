9baf898a60bca987233a29e38cc393b452c57a62
Andrii Smidnov <yukihirosm@gmail.com>
2023-05-31 13:07:07 +0300

    upd

--- scripts/test_report.py
+++ scripts/test_report.py
@@ -1,7 +1,18 @@
-from apps.home.models import StudentReport
+from apps.home.models import StudentReport, Location
 import requests
 
 def run():
-    CLIENT_MANAGERS_PROGRAMMING = list(
-    StudentReport.objects.exclude(client_manager__isnull=True).filter(start_date__gte="2022-12-26").values_list("client_manager", flat=True))
-    print(len(set(CLIENT_MANAGERS_PROGRAMMING)))
+    reports_with_unknown_regionals = StudentReport.objects.filter(regional_manager="UNKNOWN", start_date__gte="2023-05-01").all()
+    print(len(reports_with_unknown_regionals))
+    for report in reports_with_unknown_regionals:
+        location = report.location
+        try:
+            location_obj = Location.objects.get(lms_location_name=location)
+            report.territorial_manager = location_obj.territorial_manager
+            report.regional_manager = location_obj.regional_manager
+            report.business = "english"
+            report.tutor = location_obj.tutor_english
+            report.save()
+        except:
+            pass
+
